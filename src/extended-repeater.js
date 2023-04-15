52167f8ccb8a0730e2b32bca0b869f1ca793fc39
Maksim Bokhanka <isupermaximus@gmail.com>
2023-04-16 02:16:49 +0300

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    let strItem = '';
+    let strItemAdd = '';
+    let mas = []
+    let repeatTimes = options['repeatTimes'] || 1;
+    let separator = options['separator'] || '+';
+    let addition = options['addition'];
+    let additionRepeatTimes = options['additionRepeatTimes'] || 1;
+    let additionSeparator  = options['additionSeparator']||'|';
+
+    if (addition !== undefined) {
+      if (additionRepeatTimes === 1) {
+        strItemAdd = `${addition}`
+      } else {
+        let mask = [];
+        for (let i = 0; i < additionRepeatTimes; i++) {
+          mask.push(`${addition}`);
+        }
+        strItemAdd = mask.join(additionSeparator);
+      }
+    }
+    strItem = `${str}${strItemAdd}`;
+
+    for (let i = 0; i < repeatTimes; i++) {
+      mas.push(strItem)
+    }
+    if (mas.length === 1) {
+
+      return mas[0];
+    } else {
+
+      return mas.join(separator);
+    }
+
 }
+
 
 module.exports = {
   repeater
