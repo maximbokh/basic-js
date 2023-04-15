256fa7ae420f9a7a71d071e1ff2abf04bff36e26
maximbokh <isupermaximus@gmail.com>
2023-04-15 12:57:04 +0300

    feat: add solution for encode-line, matrix-elements-sum tasks

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let string = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === str[i+1]) {
+      count++;
+    } else {
+      if (count === 1) {
+        string = `${string}${str[i]}`;
+      } else {
+        string = `${string}${count}${str[i]}`;
+      }
+      count = 1;
+    }
+  }
+
+  return string;
 }
 
 module.exports = {
